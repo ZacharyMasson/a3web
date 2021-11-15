430fe675ae86b742fce35ac39176f85db3c6858e
ridleysoudack <ridley.soudack@gmail.com>
2021-11-14 21:10:49 -0500

    Merge branch 'main' of https://github.com/ZacharyMasson/a3web

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+// Mobile Navbar function, owner: Ridley
+function navBar(){
+    var nav = document.getElementById("navbarmaster");
+    if (nav.className === "navbar-menu") {
+        nav.className += " responsive";
+    } else{
+        nav.className = "navbar-menu";
+    }
+    var bar = document.getElementById("headerbar");
+    if (bar.className === "navbar-flex") {
+        bar.className += " responsive";
+    } else {
+        bar.className = "navbar-flex";
+    }
+}
