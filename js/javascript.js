015cf9a49015edf1697f86d2e29a19a885984781
K Widmeyer <kerstyne.widmeyer@flemingcollege.ca>
2021-11-25 12:42:06 -0500

    buttons

--- js/javascript.js
+++ js/javascript.js
@@ -19,3 +19,7 @@ function navBar(){
 var survey123webform = document.getElementsByName('survey123webform')[0];
 window.addEventListener("message",e=>{if(e.data){var t=JSON.parse(e.data);
 "survey123:onFormLoaded"===t.event&&t.contentHeight&&(survey123webform.parentNode.style.height=t.contentHeight+"px")&&(survey123webform.parentNode.style["padding-bottom"]="unset")}});
+
+//LinkeinButton on team page Owner:Kerstyne
+document.getElementById("Kerstyne").onclick = function(){
+    location.href="https://www.linkedin.com/in/kerstyne-widmeyer-733010200/";};
