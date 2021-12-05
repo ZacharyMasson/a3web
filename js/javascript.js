5ddf15cdff65f42b98ad1bf0f98991df4f0550b6
jadelcs <jade.lacsamana@flemingcollege.ca>
2021-12-04 19:42:42 -0500

    location page - html, css, js, hero image

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,53 @@
+// Modal box for maps on Location page, owner: Jade
+// Code from https://www.w3schools.com/howto/howto_css_modals.asp & https://codepen.io/haydmills/pen/zLraGJ
+
+// Get the modal
+var modalparent = document.getElementsByClassName("modal_multi");
+
+// Get the button that opens the modal
+var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+
+// Get the <span> element that closes the modal
+var span_close_multi = document.getElementsByClassName("close_multi");
+
+// When the user clicks the button, open the modal
+function setDataIndex() {
+for (i = 0; i < modal_btn_multi.length; i++)
+{
+modal_btn_multi[i].setAttribute('data-index', i);
+modalparent[i].setAttribute('data-index', i);
+span_close_multi[i].setAttribute('data-index', i);
+}
+}
+
+for (i = 0; i < modal_btn_multi.length; i++)
+{
+modal_btn_multi[i].onclick = function() {
+var ElementIndex = this.getAttribute('data-index');
+modalparent[ElementIndex].style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span_close_multi[i].onclick = function() {
+var ElementIndex = this.getAttribute('data-index');
+modalparent[ElementIndex].style.display = "none";
+};
+}
+window.onload = function() {
+setDataIndex();
+};
+
+window.onclick = function(event) {
+if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+modalparent[event.target.getAttribute('data-index')].style.display = "none";
+}
+
+// OLD CODE
+if (event.target === modal) {
+modal.style.display = "none";
+}
+};
+
 // Mobile Navbar function, owner: Ridley
 // Mobile Navbar function, owner: Ridley
 function navBar(){
